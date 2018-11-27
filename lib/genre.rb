ac1d79e6f23cff666dbb7efda2de55e0393408c5
Dakota Martinez <dakotaleemusic@gmail.com>
2018-11-27 00:18:58 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,12 @@
 class Genre
    attr_accessor :name, :songs, :artists
    
+   @@all = []
+   
+   def self.all 
+     @@all
+   end
+   
    def initialize(name)
       @name = name
       @songs = []

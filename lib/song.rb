8f4d3a88202476e9cb6adf9186a512425df3ddee
Dakota Martinez <dakotaleemusic@gmail.com>
2018-11-27 00:16:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end
   def initialize(name, artist, genre)
     @name = name 
     self.artist = artist 

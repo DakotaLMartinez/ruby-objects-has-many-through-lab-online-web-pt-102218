e7f3b20f949fff19e236a624d81f0f504e32fdac
Dakota Martinez <dakotaleemusic@gmail.com>
2018-11-27 00:10:26 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+  attr_accessor :songs, :genres, :name
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    @songs << song 
+    song
+  end
+  
+  def add_song(song)
+    self.songs << song unless self.songs.include?(song)
+    song.artist = self unless song.artist == self
+  end
+  
+  def genres
+    self.songs.collect do |song|
+      song.genre
+    end
+  end
+    
+end
